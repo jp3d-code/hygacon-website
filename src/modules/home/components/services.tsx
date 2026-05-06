44dc5b7162354daaf2e7e4f6b4e7c4ebae99795b
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-05-06 18:48:34 -0500

    feat: add advance of service section

--- src/modules/home/components/services.tsx
+++ src/modules/home/components/services.tsx
@@ -9,20 +9,28 @@ import services from "@/shared/data/services";
 
 export function Services() {
   return (
-    <Section>
+    <Section className="bg-muted">
       <Container>
         <SectionHeader>
           <SectionSubtitle>Lo que hacemos</SectionSubtitle>
           <SectionTitle first="Nuestros" second="Servicios" />
         </SectionHeader>
-        <div className="grid w-full items-center justify-center gap-6 lg:grid-cols-4">
+        <div className="grid w-full items-start justify-center gap-6 lg:grid-cols-4">
           {services.map((service) => (
             <div
               key={service.name}
-              className="group group-hover:translatey-1 relative flex flex-col items-start justify-start gap-5 rounded-lg border-border p-10 transition-all duration-150 ease-in-out group-hover:border-primary group-hover:shadow-lg"
+              className="group group-hover:translatey-1 relative flex flex-col items-start justify-start gap-6 rounded-lg border-border bg-background p-8 transition-all duration-150 ease-in-out group-hover:border-primary group-hover:shadow-lg"
             >
-              <div className="aspect-square">
-                
+              <div className="aspect-square rounded-md bg-accent p-3">
+                <service.icon className="h-full w-full text-secondary" />
+              </div>
+              <div className="flex flex-col items-start justify-start gap-2">
+                <span className="font-bold text-secondary text-xl uppercase">
+                  {service.name}
+                </span>
+                <span className="text-secondary/70 text-sm">
+                  {service.description}
+                </span>
               </div>
             </div>
           ))}
