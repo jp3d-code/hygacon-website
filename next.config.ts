f8964514012ebb38bf7a1a276ad4270e331a53a9
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-05-06 12:29:21 -0500

    feat: config output method of nextjs and add alternative engine

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.hlcsac.com",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
