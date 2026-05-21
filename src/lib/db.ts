b445aaed70f2e831f350a929e92d65f87cde8d2c
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-05-20 21:37:29 -0500

    feat: add betther auth config and routes

---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "@/lib/db/schema";
+import { relations } from "@/lib/db/schema";
+import { DATABASE_URL } from "@/lib/env";
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+});
+
+export const db = drizzle({
+  client: pool,
+  schema,
+  relations,
+});
+
+export { pool };
