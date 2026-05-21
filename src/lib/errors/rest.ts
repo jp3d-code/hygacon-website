4a1a472b616d96cd1036d8219b7c18c858252e89
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-05-21 12:03:56 -0500

    refactor: replace rest errors with Response-based classes

--- src/lib/errors/rest.ts
+++ src/lib/errors/rest.ts
@@ -1,49 +1,69 @@
-import { HTTPException } from "hono/http-exception";
+type ErrorBody = {
+  success: false;
+  error: string;
+};
 
-export class UnauthorizedError extends HTTPException {
+export class HttpResponseError extends Response {
+  constructor(status: number, message: string) {
+    super(
+      JSON.stringify({
+        success: false,
+        error: message,
+      } satisfies ErrorBody),
+      {
+        status,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+}
+
+export class UnauthorizedError extends HttpResponseError {
   constructor(message = "Usted no está autorizado para realizar esta acción") {
-    super(401, { message });
+    super(401, message);
   }
 }
 
-export class BadRequestError extends HTTPException {
-  constructor(message: string) {
-    super(400, { message });
+export class BadRequestError extends HttpResponseError {
+  constructor(message = "Solicitud inválida") {
+    super(400, message);
   }
 }
 
-export class NotFoundError extends HTTPException {
-  constructor(message: string) {
-    super(404, { message });
+export class NotFoundError extends HttpResponseError {
+  constructor(message = "Recurso no encontrado") {
+    super(404, message);
   }
 }
 
-export class ForbiddenError extends HTTPException {
+export class ForbiddenError extends HttpResponseError {
   constructor(message = "No tiene permisos para acceder a este recurso") {
-    super(403, { message });
+    super(403, message);
   }
 }
 
-export class InternalServerError extends HTTPException {
+export class InternalServerError extends HttpResponseError {
   constructor(message = "Error interno del servidor") {
-    super(500, { message });
+    super(500, message);
   }
 }
 
-export class ConflictError extends HTTPException {
-  constructor(message: string) {
-    super(409, { message });
+export class ConflictError extends HttpResponseError {
+  constructor(message = "Conflicto en la operación") {
+    super(409, message);
   }
 }
 
-export class PayloadTooLargeError extends HTTPException {
+export class PayloadTooLargeError extends HttpResponseError {
   constructor(message = "El archivo es demasiado grande") {
-    super(413, { message });
+    super(413, message);
   }
 }
 
-export class UnsupportedMediaTypeError extends HTTPException {
+export class UnsupportedMediaTypeError extends HttpResponseError {
   constructor(message = "Tipo de archivo no soportado") {
-    super(415, { message });
+    super(415, message);
   }
 }
